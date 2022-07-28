25303b2feddfdeca0a4ba2e6cbd91a9f2afe9a66
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-07-28 22:12:51 +0300

    new commit

--- components/Header/Navbar.js
+++ components/Header/Navbar.js
@@ -1,8 +1,8 @@
 const styles = {
-  nav: 'absolute top-0 left-0 z-50 w-full px-[135px]',
-  navMain: 'py-8 lg:py-5 px-5 flex justify-between max-w-screen-1.5lg  m-auto',
+  nav: 'w-full px-[135px]',
+  navMain: 'py-8 lg:py-5 px-5 flex items-center justify-between max-w-screen-1.5lg  m-auto',
   ul: 'w-2/5 hidden md:block py-3 justify-between',
-  li: 'parent-item px-5 py-1 text-[17px] text-white inline-block focus-within:ring-0',
+  li: 'parent-item text-[17px] px-5 text-white inline-block focus-within:ring-0',
   buttonSection: 'hidden md:flex w-2/5 py-1 text-base text-white flex-wrap content-center justify-end items-center space-x-4 lg:flex',
 }
 
@@ -33,7 +33,7 @@ const Navbar = () => {
               </a>
           </li>
         </ul>
-        <a className="self-center text-white text-[32px] font-bold font-sans" href="#">smartHome</a>
+        <a className="text-white text-[32px] font-bold font-sans" href="#">smartHome</a>
         <div className={styles.buttonSection}>
           <a href="#" className="w-auto py-3 mr-5 whitespace-pre hover:underline">Login</a>
           <button className="w-auto px-6 py-3 rounded-md btn btn--swap shadow-lg bg-white">
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Head from 'next/head';
 import React from 'react'
-import Header from '../components/Header/Header'
 import Navbar from '../components/Header/Navbar'
 import Hero from '../components/Header/Hero'
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items- justify-flex-start">
+    <div className="flex min-h-screen flex-col items-center justify-flex-start">
+      <div className="bg-regal-blue absolute z-[-1] w-full h-[900px]"></div>
+      <Head>
+        <title>App Landing | A mobile app landing page</title>
+        <meta name="Description" content="React next landing page" />
+        <meta name="theme-color" content="#1E5AFA" />
+      </Head>
       <Navbar/>
       <Hero/>
     </div>
