f08742d8f94ce0cf40b5b47095f4d66f70e22079
Harun Inam <45066973+HarunInam@users.noreply.github.com>
2022-07-29 01:25:00 +0300

    Deneme

--- components/Header/Clients.js
+++ components/Header/Clients.js
@@ -4,7 +4,12 @@ const styles = {
 
 const Client = () => {
     return (
-        <>
-        </>
+        <section>
+            <h1 className="text-[85px] text-center font-bold">Our Clients</h1>
+            <div className="">
+
+            </div>
+
+        </section>
     )
 }
