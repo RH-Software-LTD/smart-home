de33f1f32be0e56441e9fa81f2337b19f282cce0
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-07-30 14:10:05 +0300

    client-side logo paddings changed.

--- components/Clients/Clients.js
+++ components/Clients/Clients.js
@@ -1,25 +1,25 @@
 
 const styles = {
-    columns: 'mx-auto grid grid-cols-4 gap-4',
-    logos: 'border-1 object-fit justify-center p-6 mb-6 shadow-lg'
+    columns: 'mx-auto grid grid-cols-4 gap-4 mb-6',
+    logos: 'border-2 object-fit p-6 shadow-lg rounded-md'
 
 }
 
 const Clients = () => {
   return (
-    <section className="max-w-[1320px]">
+    <section className="max-w-[1320px] w-full px-32">
         <div className={styles.columns}>
             <div className={styles.logos}>
-                <img className="mx-auto h-12" src="/logo1.png" alt="logo1" />
+                <img className="mx-auto h-16" src="/logo1.png" alt="logo1" />
             </div>
             <div className={styles.logos}>
-                <img className="mx-auto h-12" src="/logo2.png" alt="logo2" />
+                <img className="mx-auto h-16" src="/logo2.png" alt="logo2" />
             </div>
             <div className={styles.logos}>
-                <img className="mx-auto h-12" src="/logo3.png" alt="logo3" />
+                <img className="mx-auto h-16" src="/logo3.png" alt="logo3" />
             </div>
             <div className={styles.logos}>
-                <img className="mx-auto h-12" src="/logo4.png" alt="logo4" />
+                <img className="mx-auto h-16" src="/logo4.png" alt="logo4" />
             </div>
         </div>
         <div className={styles.columns}>
