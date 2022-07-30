1bbb08b9fdfb843534fb7aa5d4758b1e2aa2e1e4
Harun Inam <45066973+HarunInam@users.noreply.github.com>
2022-07-30 13:17:25 +0300

    Küçük şeyler

--- components/Clients/Clients.js
+++ components/Clients/Clients.js
@@ -8,32 +8,32 @@ const styles = {
 const Clients = () => {
   return (
     <section className="max-w-[1320px]">
-        <div class={styles.columns}>
+        <div className={styles.columns}>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo1.png" alt="logo1" />
+                <img className="mx-auto h-12" src="/logo1.png" alt="logo1" />
             </div>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo2.png" alt="logo2" />
+                <img className="mx-auto h-12" src="/logo2.png" alt="logo2" />
             </div>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo3.png" alt="logo3" />
+                <img className="mx-auto h-12" src="/logo3.png" alt="logo3" />
             </div>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo4.png" alt="logo4" />
+                <img className="mx-auto h-12" src="/logo4.png" alt="logo4" />
             </div>
         </div>
-        <div class={styles.columns}>
+        <div className={styles.columns}>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo5.png" alt="logo5" />
+                <img className="mx-auto h-12" src="/logo5.png" alt="logo5" />
             </div>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo6.png" alt="logo6" />
+                <img className="mx-auto h-12" src="/logo6.png" alt="logo6" />
             </div>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo7.png" alt="logo7" />
+                <img className="mx-auto h-12" src="/logo7.png" alt="logo7" />
             </div>
             <div className={styles.logos}>
-                <img class="mx-auto h-12" src="/logo8.png" alt="logo8" />
+                <img className="mx-auto h-12" src="/logo8.png" alt="logo8" />
             </div>
         </div>
     </section>
