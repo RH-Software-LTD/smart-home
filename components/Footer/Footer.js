fbf9e03ead69128f00717589e8b58995a6c3b20e
Harun Inam <45066973+HarunInam@users.noreply.github.com>
2022-07-31 20:26:13 +0300

    Footer v2

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,6 @@
 const styles = {
-    footer: 'container max-w-[1320px] py-10 bg-white w-full mx-auto px-4',
-    flex: "mx-4 flex flex-wrap pt-20 pb-6",
+    footer: 'relative container py-10 w-[1320px] px-4',
+    flex: "flex flex-wrap gap-16 justify-center",
 
 }
 
@@ -9,7 +9,7 @@ const Footer = () => {
     <>
       <section className={styles.footer}>
         <div className={styles.flex}>
-          <div className="mb-12 px-4 gap-3 w-1/4">
+          <div className="mb-12 p-6 w-4/12">
               <h2 className="text-[32px] text-black text-bold">smartHome</h2>
               <p className="text-gray-500 mt-4 text-[16px] font-medium w-[334px]">Automate your entire healthcare hiring, onboarding and compliance with a true technology platform.</p>
               <div className="flex w-[334px] mt-8 flex-wrap">
@@ -21,7 +21,7 @@ const Footer = () => {
                 </div>
               </div>
           </div>
-          <div className="px-4 w-1/4">
+          <div className="w-2/12">
             <h2 className="uppercase text-[24px] mb-10 font-semibold">Categories</h2>
             <ul>
               <li className="mb-5">Product Management</li>
@@ -32,7 +32,7 @@ const Footer = () => {
               <li>Customer Support</li>
             </ul>
           </div>
-          <div className="px-4 w-1/4">
+          <div className="w-2/12">
             <h2 className="uppercase text-[24px] mb-10 font-semibold">About</h2>
             <ul>
               <li className="mb-5">About Us</li>
@@ -43,7 +43,7 @@ const Footer = () => {
               <li>The Team</li>
             </ul>
           </div>
-          <div className="px-4 w-1/4">
+          <div className="w-2/12">
             <h2 className="uppercase text-[24px] mb-10 font-semibold">Follow Us</h2>
             <ul>
               <li className="mb-5">Facebook</li>
