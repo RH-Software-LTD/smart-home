f71f99d5663ee516ebfe365c366b1c3b849330ba
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-08-10 19:30:03 +0300

    about section layout upgrades

--- components/About/About.js
+++ components/About/About.js
@@ -3,14 +3,14 @@ import Card from "./Card";
 const styles = {
   section: 'relative py-4 flex flex items-center flex-col justify-center ',
   container: 'w-4/5 flex items-center flex-col justify-center',
-  header: 'text-6xl font-bold py-12 text-black',
+  header: 'text-4xl text-center font-bold py-12 text-black',
   vector: 'absolute w-1/2 right-0 top-[-200px]',
   vectorImg: 'w-full'
 }
 
 const About = () => {
   return (
-    <section className={styles.section}>
+    <section id="About" className={styles.section}>
       <div className={styles.vector}>             
       <img src="/vectorLine.png" className={styles.vectorImg} alt='vectorLine'></img>
       </div>
