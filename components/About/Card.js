a35a0b9f12f38574b88358dba7accdc052deb27f
Recai Çakıroğlu <recai.cakiroglu@startuphero.es>
2022-08-10 19:30:26 +0300

    about card section upgraded with css

--- components/About/Card.js
+++ components/About/Card.js
@@ -1,17 +1,18 @@
 const styles = {
-  container: "flex flex-col py-10 md:space-x-12 w-full justify-between md:flex-row",
+  containerLeft: "flex flex-col md:space-x-20 w-full justify-between md:flex-row",
+  containerRight: "flex flex-col md:space-x-20 w-full justify-between md:flex-row flex-col-reverse",
   phone: "flex justify-center items-center md:w-1/2",
-  infoBox: "md:w-1/2",
-  header: "pt-32 text-aboutHeader font-semibold tracking-[2px] text-[#000F37]",
-  info: "py-10 text-xl font-normal text-[#575757]",
+  infoBox: "md:w-1/2 flex flex-col items-center md:items-start",
+  header: "pt-8 md:pt-32 text-3xl font-semibold tracking-[2px] text-[#000F37]",
+  info: "py-10 text-xl font-normal text-[#C2C2C2]",
 };
 
 const Card = () => {
   return (
     <>
-      <section className={styles.container}>
+      <section className={styles.containerLeft}>
         <div className={styles.phone}>
-          <img src="phone-rectangle.png"></img>
+          <img src="firstPhone.png"></img>
         </div>
         <div className={styles.infoBox}>
           <h2 className={styles.header}>Smart Home’s Smart Services</h2>
@@ -22,11 +23,11 @@ const Card = () => {
             mile. An pasture he himself believe ferrars besides cottage.
           </p>
           <button className="w-auto px-10 py-4 rounded-md border-2 btn btn--swap text-black shadow-lg hover:text-white hover:bg-[#000F37]">
-              <span>Learn More</span>
-            </button>
+            <span>Learn More</span>
+          </button>
         </div>
       </section>
-      <section className={styles.container}>
+      <section className={styles.containerRight}>
         <div className={styles.infoBox}>
           <h2 className={styles.header}>What app can do to your Appliences?</h2>
           <p className={styles.info}>
@@ -36,16 +37,16 @@ const Card = () => {
             mile. An pasture he himself believe ferrars besides cottage.
           </p>
           <button className="w-auto px-10 py-4 rounded-md border-2 btn btn--swap text-black shadow-lg hover:text-white hover:bg-[#000F37]">
-              <span>Learn More</span>
+            <span>Learn More</span>
           </button>
         </div>
         <div className={styles.phone}>
-          <img src="phone-rectangle.png"></img>
+          <img src="secondPhone.png"></img>
         </div>
       </section>
-      <section className={styles.container}>
+      <section className={styles.containerLeft}>
         <div className={styles.phone}>
-          <img src="phone-rectangle.png"></img>
+          <img src="thirdPhone.png"></img>
         </div>
         <div className={styles.infoBox}>
           <h2 className={styles.header}>Control Electric Appliences</h2>
@@ -56,9 +57,9 @@ const Card = () => {
             mile. An pasture he himself believe ferrars besides cottage.
           </p>
           <button className="w-auto px-10 py-4 rounded-md border-2 btn btn--swap text-black shadow-lg hover:text-white hover:bg-[#000F37]">
-              <span>Learn More</span>
-            </button>
-          </div>
+            <span>Learn More</span>
+          </button>
+        </div>
       </section>
     </>
   );
